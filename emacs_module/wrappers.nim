ee534fce1889a3c3451028f01b96582c73512f37
Kaushal Modi <kaushal.modi@gmail.com>
2018-06-25 13:07:04 -0400

    Add assertSuccessExitStatus proc

--- emacs_module/wrappers.nim
+++ emacs_module/wrappers.nim
@@ -5,17 +5,28 @@ import emacs_module
 
 type
   UserError = object of Exception
+  NonZeroExitStatus = object of Exception
+
+proc assertSuccessExitStatus*(env: ptr emacs_env) =
+  ## Raise an exception if the non-local exit check does not return
+  ## ``emacs_funcall_exit_return``.
+  let exitStatus: emacs_funcall_exit = env.non_local_exit_check(env)
+  if not (exitStatus == emacs_funcall_exit_return):
+    raise newException(NonZeroExitStatus, "Non-zero exit status " & $exitStatus & " (" & $ord(exitStatus) & ") detected")
 
 # http://phst.github.io/emacs-modules.html#copy_string_contents
 proc copyStringContents*(env: ptr emacs_env; elispStr: emacs_value): string =
   ## Copy Emacs-Lisp string ``elispStr`` to a Nim string and return it.
-  var l: ptrdiff_t
+  var
+    l: ptrdiff_t
   # Get the length of the elisp string elispStr. It's num chars + 1
   # because it includes the null-termination too.
-  if (env.copy_string_contents(env, elispStr, nil, addr l)):
-    # So the actual string length is l-1. Allocate that much space for
-    # the str string in Nim.
-    var str = newString(l-1)
-    # *Now* copy the elisp string elispStr to Nim string str.
-    if (env.copy_string_contents(env, elispStr, addr str[0], addr l)):
-      return str
+  discard env.copy_string_contents(env, elispStr, nil, addr l)
+  env.assertSuccessExitStatus
+  # So the actual string length is l-1. Allocate that much space for
+  # the str string in Nim.
+  var str = newString(l-1)
+  # *Now* copy the elisp string elispStr to Nim string str.
+  discard env.copy_string_contents(env, elispStr, addr str[0], addr l)
+  env.assertSuccessExitStatus
+  return str
